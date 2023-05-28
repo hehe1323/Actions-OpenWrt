0c96c1398ac912e2bf8bca90fc6ca8a669ee6743
hehe1323 <chirongda@hotmail.com>
2023-05-28 21:30:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git oscamapp https://github.com/ntlf9t/luci-app-oscam.git' >>feeds.conf.default
+echo 'src-git oscam https://github.com/ntlf9t/openwrt_oscam.git' >>feeds.conf.default
